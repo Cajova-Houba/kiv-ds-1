cefccdb3a5d8a28612be08e6fe13bc2f94c5a8cb
Zdenek Vales <tkmushroomer@gmail.com>
2019-11-01 17:03:08 +0100

    #3: DB connection and base schema added to bank server.

--- src/bank/bank.py
+++ src/bank/bank.py
@@ -15,19 +15,76 @@
 #
 
 from bottle import Bottle, template, request, HTTPResponse
+import mysql.connector
+
+class DbConnector:
+	"""
+	Implementation of DB connector.
+	"""
+	
+	def __init__(self, host = 'localhost', user = 'root', password = 'r00t', schema = 'bank_server', account_id = 1):
+		self._host = host
+		self._user = user
+		self._password = password
+		self._schema = schema
+		self._account_id = account_id
+		self._connection = self._get_connection()
+	
+	def _get_connection(self):
+		return mysql.connector.connect(
+			host=self._host,
+			user=self._user,
+			passwd=self._password,
+			database=self._schema
+		)
+	
+	def _perform_update_query(self, query, amount):
+		"""
+		Executes and commits the update query.
+		"""
+		db = self._connection
+		cursor = db.cursor()
+		cursor.execute(query, (amount, self._account_id))
+		db.commit()
+		cursor.close()
+	
+	def close_connection(self):
+		self._connection.close()
+	
+	def credit_money(self, amount):
+		"""
+		Credits given amount of money to the account.
+		"""
+		print("crediting %d" % amount)
+		self._perform_update_query(
+			"update account set balance = balance + %s where id = %s;",
+			amount
+		)
+		
+	def debit_money(self, amount):
+		"""
+		Debits given amount of money from the account.
+		"""
+		print("debiting %d" % amount)
+		self._perform_update_query(
+			"update account set balance = balance - %s where id = %s;",
+			amount
+		)
+		
 
 class Bank:
 	"""
 	Implementation of the bank server.
 	"""
 	
-	def __init__(self, host, port, debug):
+	def __init__(self, host, port, debug, db_connector):
 		"""
 		Initializes this server with given values.
 		"""
 		self._host = host
 		self._port = port
 		self._debug = debug
+		self._db_connector = db_connector
 		self._app = Bottle()
 		self._route()
 	
@@ -50,7 +107,7 @@ class Bank:
 		"""
 		transaction = request.json
 		print("Credit: " + str(transaction))
-		# todo call DB
+		self._db_connector.credit_money(transaction["amount"])
 		return HTTPResponse(status = 200)
 	
 	def _debit(self):
@@ -59,7 +116,7 @@ class Bank:
 		"""
 		transaction = request.json
 		print("Debit: " + str(transaction))
-		# todo call DB
+		self._db_connector.debit_money(transaction["amount"])
 		return HTTPResponse(status = 200)
 	
 		
@@ -68,8 +125,10 @@ def main():
 	"""
 	Main method of the script, starts the server.
 	"""
-	bank = Bank('0.0.0.0', 8100, True)
+	db_connector = DbConnector()
+	bank = Bank('0.0.0.0', 8100, True, db_connector)
 	bank.start()
+	db_connector.close_connection()
 		
 
 # Script body
